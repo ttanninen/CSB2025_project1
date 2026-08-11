33634f19e1bebe263f695f383da10af168fcfb64
ttanninen <teemu@TEPU.localdomain>
2026-08-12 00:21:21 +0300

    Update models with string prints

--- flawed_site/assignment_portal/models.py
+++ flawed_site/assignment_portal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=70, default = "")
+    number = models.CharField(max_length = 8)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length = 70)
+    code = models.CharField(max_length = 8, default="00000000")
+
+    def __str__(self):
+        return self.name
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student} enrolled in {self.course}"
